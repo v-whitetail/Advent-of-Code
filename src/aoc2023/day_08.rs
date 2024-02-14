b547eb0b37831028fbc83c3589ddc5bcae8e166b
v <white.tail.millwork@gmail.com>
2024-02-13 21:25:05 -0500

    🦌 day 8: part 2 - this loop do be infinite 🦌

--- src/aoc2023/day_08.rs
+++ src/aoc2023/day_08.rs
@@ -4,6 +4,7 @@ use std::fs::*;
 use std::cmp::*;
 use std::ops::*;
 use std::sync::*;
+use std::borrow::*;
 use std::collections::*;
 use itertools::Itertools;
 use nom::{
@@ -21,18 +22,58 @@ use crate::aoc2023::Input;
 
 
 pub const DAY_8: &str = file!();
+
+
+pub fn part_two(input: Input) -> Result<u64> {
+    let input_string = input.read();
+    let (_, instructions) = Instructions::parse(&input_string)
+        .map_err(|err| anyhow!(err.to_owned()))?;
+    let mut positions = instructions.map.data
+        .iter()
+        .map( |(position, _)| position.clone() )
+        .filter( |position| position.as_bytes()[2] == b'A' )
+        .collect::<Vec<_>>();
+    let mut targets = instructions.map.data
+        .iter()
+        .map( |(position, _)| position.clone() )
+        .filter( |position| position.as_bytes()[2] == b'Z' )
+        .collect::<Vec<_>>();
+    positions.sort();
+    targets.sort();
+
+    let mut count = 0u64;
+    for direction in instructions.directions.iter().cycle() {
+        if positions == targets { break; };
+        for position in positions.iter_mut() {
+            *position = match direction {
+                Direction::L => instructions.map.data[position][0].clone(),
+                Direction::R => instructions.map.data[position][1].clone(),
+            };
+        };
+        positions.sort();
+        count += 1;
+    };
+    Ok(count)
+}
+#[test]
+fn test_part_two() {
+    let ans = part_two(Input::new(DAY_8).test()).unwrap();
+    assert_eq!(6, ans);
+}
+
+
 const SOURCE: &str = "AAA";
 const TARGET: &str = "ZZZ";
 
 
-pub fn part_one(input: Input) -> Result<u64> {
+pub fn part_one(input: Input, source: &str, target: &str) -> Result<u64> {
     let input = input.read();
     let (_, instructions) = Instructions::parse(&input)
         .map_err(|err| anyhow!(err.to_owned()))?;
     let mut count = 0u64;
-    let mut position = SOURCE.into();
+    let mut position = source.into();
     for direction in instructions.directions.iter().cycle() {
-        if position == TARGET { break; };
+        if position == target { break; };
         position = match direction {
             Direction::L => &instructions.map.data[position][0],
             Direction::R => &instructions.map.data[position][1],
@@ -43,12 +84,12 @@ pub fn part_one(input: Input) -> Result<u64> {
 }
 #[test]
 fn test_part_one() {
-    let ans = part_one(Input::new(DAY_8).test()).unwrap();
+    let ans = part_one(Input::new(DAY_8).test(), SOURCE, TARGET).unwrap();
     assert_eq!(2, ans);
 }
 #[test]
 fn ans_part_one() {
-    let ans = part_one(Input::new(DAY_8)).unwrap();
+    let ans = part_one(Input::new(DAY_8), SOURCE, TARGET).unwrap();
     assert_eq!(21797, ans);
 }
 
@@ -125,12 +166,12 @@ impl Turn {
             })(s)
     }
     fn line(s: &str) -> IResult<&str, (&str, (&str, &str))> {
-        separated_pair(alpha1, tag(" = "), Self::duple)(s)
+        separated_pair(alphanumeric1, tag(" = "), Self::duple)(s)
     }
     fn duple(s: &str) -> IResult<&str, (&str, &str)> {
         delimited(tag("("), Self::comma, tag(")"))(s)
     }
     fn comma(s: &str) -> IResult<&str, (&str, &str)> {
-        separated_pair(alpha1, tag(", "), alpha1)(s)
+        separated_pair(alphanumeric1, tag(", "), alphanumeric1)(s)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use advent_of_code::aoc2023::*;
 use anyhow::Result;
 
 fn main() -> Result<()>{
-    let ans = crate::day_08::part_one(Input::new(crate::day_08::DAY_8))?;
-    println!("{ans:#?}");
+    let ans = crate::day_08::part_two(Input::new(crate::day_08::DAY_8))?;
     Ok(())
 }
