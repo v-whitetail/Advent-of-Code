85d66f86fa1b3abd82a237fbd1c967987ac53e98
v-whitetail <white.tail.millwork@gmail.com>
2024-12-23 13:32:14 -0500

    cleaning

--- 2024/day_02/target/debug/deps/day_02-70020210221c7c16.d
+++
@@ -1,5 +0,0 @@
-/home/v/crystalPeak/advent-of-code/2024/day_02/target/debug/deps/libday_02-70020210221c7c16.rmeta: src/main.rs
-
-/home/v/crystalPeak/advent-of-code/2024/day_02/target/debug/deps/day_02-70020210221c7c16.d: src/main.rs
-
-src/main.rs:
